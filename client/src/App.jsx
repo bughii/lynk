9dfd1b64379484f48b59a713f9cee3f9f358cafc
bughii <162333595+bughii@users.noreply.github.com>
2024-09-15 18:51:38 +0200

    Impostato il router-dom e creato le prime pagine

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,19 @@
 import React from "react";
 import { Button } from "@/components/ui/button";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Authentication from "./pages/authentication";
+import Chat from "./pages/chat";
+import Profile from "./pages/profile";
 
 const App = () => {
   return (
-    <div>
-      <Button>Click me</Button>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/authentication" element={<Authentication />} />
+        <Route path="/chat" element={<Chat />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
